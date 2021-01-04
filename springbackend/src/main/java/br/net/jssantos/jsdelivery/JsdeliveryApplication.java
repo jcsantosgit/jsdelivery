c545301d26a95b680dfb89c07432152efc80ee20
Joao Santos <jcfsantos@apoioprodesp.sp.gov.br>
2021-01-04 18:37:31 -0300

    criando o projeto

---
+++ springbackend/src/main/java/br/net/jssantos/jsdelivery/JsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.net.jssantos.jsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdeliveryApplication.class, args);
+	}
+
+}
